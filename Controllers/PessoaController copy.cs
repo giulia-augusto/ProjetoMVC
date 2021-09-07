c3c8bed7f08ef5c5cc8d128e3a1c7f841ab853c8
Giulia Augusto <giuliaaa2010@gmail.com>
2021-09-07 20:30:59 -0300

    Primeiro commit

---
+++ Controllers/PessoaController copy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult PessoaData()
+        {
+            var pessoa = new Pessoa {
+                Nome = "Giulia Augusto",
+                Endereco = "Rua A, 123 - Perdões",
+                Email = "giulia@gmail.com",
+                Contato = "035987654321"
+            };
+
+            return View(pessoa);
+        }
+
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjetoMVC.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+        public string Contato { get; set; }
+    }
+}
